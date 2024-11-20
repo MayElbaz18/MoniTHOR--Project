a58b783b7aac72b45e5da387e130990672513989
MayElbaz18 <may.elbaz12@gmail.com>
2024-11-20 15:45:13 +0200

    Alert for login

--- static/script.js
+++ static/script.js
@@ -16,6 +16,7 @@ async function Login(UserName, Password) {
     console.log(data)
     
     if (data.includes("Login Successful")) {
+        alert("Loged In Successfully")
         // Redirect to the dashboard page after successful login
         window.location.href = '/0';
     } else {
