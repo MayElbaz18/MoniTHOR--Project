b56c8b1c53f7142cee10d0d9b060c232e5d7798a
MayElbaz18 <may.elbaz12@gmail.com>
2024-12-01 18:03:10 +0200

    fix

--- app.py
+++ app.py
@@ -295,6 +295,7 @@ def remove_domain(domainName):
     return "Error: Domain could not be removed"
 
 
+
 # usage : http://127.0.0.1:8080/bulk_upload/.%5Cuserdata%5CDomains_for_upload.txt 
 # using  %5C instaed of  "\"  
 # in UI put    ./userdata/Domains_for_upload.txt
