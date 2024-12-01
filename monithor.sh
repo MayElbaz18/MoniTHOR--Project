b7e76132db324219cf47ca9890de45e49b13110b
MayElbaz18 <may.elbaz12@gmail.com>
2024-12-01 20:46:26 +0200

    fix

--- monithor.sh
+++ monithor.sh
@@ -4,7 +4,7 @@ sudo apt install git -y
 git clone https://github.com/MayElbaz18/MoniTHOR--Project.git
 cd MoniTHOR--Project
 sudo apt install python3-pip
-pip3 install -r requirements.txt --break-system-packages --ignore-installed
+pip install -r requirements.txt --break-system-packages --ignore-installed
 sudo chmod -R 777 .
 sudo bash -c 'cat << EOF > /etc/systemd/system/MoniTHOR.service [Unit] Description=MoniTHOR instance, Flask application for domains monitoring After=network.target [Service] User=ubuntu Group=ubuntu WorkingDirectory=/home/ubuntu/MoniTHOR--Project Environment="/home/ubuntu/MoniTHOR--Project/.env" ExecStart=/usr/bin/python3 /home/ubuntu/MoniTHOR--Project/app.py [Install] WantedBy=multi-user.target EOF'
 sudo systemctl daemon-reload
